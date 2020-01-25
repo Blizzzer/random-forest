51b83cf80ed574764db7be8b8f9039a4de9134eb
Krzysztof Apolinarski <k.apolinarski@gmail.com>
2020-01-25 14:45:53 +0100

    Initial commit - import data

---
+++ main.R
@@ -0,0 +1,9 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: kapol
+# Created on: 18.01.2020
+# http://archive.ics.uci.edu/ml/machine-learning-databases/acute/diagnosis.data
+print("Hello world")
+mydata <- read.delim2("diagnosis.csv", header=FALSE, sep=";", dec = ",", quote = "")
+print(mydata)
+
