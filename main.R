4f0d586457c5fc796615ac8a9ea65cc16feae46b
Krzysztof Apolinarski <k.apolinarski@gmail.com>
2020-01-25 17:26:12 +0100

    decision tree prediction

--- main.R
+++ main.R
@@ -6,20 +6,42 @@
 
 # print("Hello world")
 
-test_rows_number <- 20
-learning_rows_number <- 100
-
+training_percent <- 0.75
+seed <- 234
 
 mydata <- read.table("diagnosis.csv", header=FALSE, sep=";", dec = ",", quote = "")
 colnames(mydata) <- c("temp", "nausea", "lumbar_pain", "urine_pushing", "micturition_pain", "burning", "dec_inflamation", "dec_nephritis")
+mydata$dec_inflamation = ifelse(mydata$dec_inflamation == "yes", 1, 0)
+mydata$dec_nephritis = ifelse(mydata$dec_nephritis == "yes", 1, 0)
 str(mydata)
-number_of_rows <- nrow(mydata)
 
-if(test_rows_number + learning_rows_number != number_of_rows) {
-  stop("invalid split of rows")
-}
+train_size <- floor(0.75 * nrow(mydata))
+set.seed(seed)
 
+train_ind <- sample(seq_len(nrow(mydata)), size = train_size)
 
-print(paste0("Number of rows: ", nrow(mydata)));
+train <- mydata[train_ind, ]
+test <- mydata[-train_ind, ]
 
+str(train)
 
+str(test)
+
+targ <- "dec_inflamation"
+preds <- c("temp", "nausea", "lumbar_pain", "urine_pushing", "micturition_pain", "burning")
+
+dtree <- rpart::rpart(formula = train[,targ] ~ ., data = train[,preds])
+
+# rpart.plot::rpart.plot(dtree)
+
+predictions <- predict(dtree, test)
+predictions <- ifelse(predictions >= 0.5, 1, 0)
+print(predictions)
+predictions <- as.numeric(as.matrix(predictions))
+
+reference <- test$dec_inflamation
+
+table <- table(factor(predictions, levels=min(predictions):max(predictions)),
+      factor(predictions, levels=min(predictions):max(predictions)))
+
+print(table)
